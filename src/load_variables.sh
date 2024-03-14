b2e976459b4011a969df8e2d4cf0c5156a2fb493
Matheus Damasceno <matheushrd@hotmail.com>
2024-03-14 17:38:00 -0300

    update: filename variable

--- src/load_variables.sh
+++ src/load_variables.sh
@@ -3,7 +3,7 @@
 # Set environment variables
 export FILE_ID="1ig2ngoXFTxP5Pa8muXo02mDTFexZzsis"
 export CREDS_PATH="../develop/creds.json"
-export FILENAME="<YOUR_FILENAME>"
+export FILENAME="tweets"
 
 # Optionally, run your Python script after setting the environment variables
 # python your_script.py
